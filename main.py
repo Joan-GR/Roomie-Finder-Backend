fe1d5e914c61c9c1122c0cfac1e2b39a8e1c7ea2
Joan-GR <joanguerson@gmail.com>
2026-07-01 15:15:16 -0300

    cors y req de vercel para empezar conexion frontend

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
-from routers import users, publicaciones, auth
-from routers import postulaciones
-from routers import fotos
+from routers import users, publicaciones, auth, postulaciones, fotos
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # url de front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(publicaciones.router)
-app.include_router(auth.router)
 app.include_router(postulaciones.router)
 app.include_router(fotos.router)
 
-
 @app.get("/")
 def root():
-    return {"message": "cuandoyolavi"}
+    return {"message": "Roomie Finder API funcionando"}
